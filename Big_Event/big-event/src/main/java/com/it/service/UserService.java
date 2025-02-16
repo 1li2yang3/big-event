5c7c8f2e61d883db87104529fbda40538238457d
yyl <3237918584@qq.com>
2025-02-16 20:22:14 +0800

    Delete Big_Event/big-event directory

--- Big_Event/big-event/src/main/java/com/it/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.it.service;
-
-import com.it.pojo.User;
-
-public interface UserService {
-    User findByUserName(String username);
-
-    void register(String username, String password);
-}
