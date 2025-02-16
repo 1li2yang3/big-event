e088a9f524257856e5621ac0e1a8797d3fad6d22
yyl <3237918584@qq.com>
2025-02-16 20:19:13 +0800

    Add files via upload

---
+++ Big_Event/big-event/src/main/java/com/it/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.it.controller;
+
+import com.it.pojo.Result;
+import com.it.pojo.User;
+import com.it.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public Result register(String username, String password) {
+        User u=userService.findByUserName(username);
+        if(u==null){
+            userService.register(username,password);
+            return Result.success();
+        }
+        else{
+            return Result.error("用户名已被占用");
+        }
+    }
+}
